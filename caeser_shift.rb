857e34824c2d69cab536549800db17c43c59d796
jo-and75 <jaotika@gmail.com>
2024-06-30 12:50:48 +0300

    Commit code

---
+++ caeser_shift.rb
@@ -0,0 +1,12 @@
+def caesar_cipher(string, key)
+  string.split('').map { |character|
+    if c =~ /\W/
+      c
+    else
+      key.times.inject(character) { |character, _| character.next }[-1]
+    end
+  }.join
+end 
+
+puts caesar_cipher("What a string",5) 
+
